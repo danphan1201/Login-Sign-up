742abcf2abde3954092bba1afeb9514c8d0ff69c
DanPhan <danphan1201@gmail.com>
2024-08-19 09:21:14 +0700

    login and signup validate

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Components/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import AuthForm from './AuthForm';
+import './LoginSignup.css'; 
+
+const Login = () => {
+  return <AuthForm isLogin={true} />;
+};
+
+export default Login;
